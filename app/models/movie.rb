cbc4720c8881830502094af87326b65225359a9e
Nancy-techprep <117962831+Nancy-techprep@users.noreply.github.com>
2023-05-11 16:10:25 +0000

    updated associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,9 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  belongs_to(:director) #we are dropping the method argument paranthesis as it isn't needed
+  has_many :characters
+
+  has_many :cast, :through => :characters, :source => :actor
+  # has_many (:cast, { :through => :characters, :source => :actor })
 end
